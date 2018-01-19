152e9a4d09562fdca6a4465f53b4f3d121de3a06
ankitasaxena <saxenaankita2010@gmail.com>
2018-01-18 23:33:24 -0500

    Added java file for hello word example

---
+++ src/com/ankita/overview/HelloWorld.java
@@ -0,0 +1,9 @@
+package com.ankita.overview;
+
+public class HelloWorld 
+{
+	public static void hw()
+	{
+		System.out.println("Hello World");
+	}
+}
---
+++ src/com/ankita/overview/ThisIsMain.java
@@ -0,0 +1,10 @@
+package com.ankita.overview;
+
+public class ThisIsMain 
+{
+	public static void main(String[] args)
+	{
+		HelloWorld h = new HelloWorld();
+		h.hw();
+	}
+}
